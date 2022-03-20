31a8c83d07324268f3c073f2f7003328e5e847c4
vyhuholl <olga-p-98@mail.ru>
2022-03-20 23:37:45 +0300

    fixed another error in metric.py

--- metric.py
+++ metric.py
@@ -241,7 +241,14 @@ def main(
             if os.path.isfile(results_file):
                 with open(results_file, "a") as results:
                     results.write(
-                        " | ".join([str(sta), str(cs), str(fl), f"**{js}**"])
+                        " | ".join(
+                            [
+                                f"{np.mean(sta):.2f}",
+                                f"{np.mean(cs):.2f}",
+                                f"{np.mean(fl):.2f}",
+                                f"**{np.mean(js):.2f}**",
+                            ]
+                        )
                     )
             else:
                 raise ValueError("Results file is a directory!")
