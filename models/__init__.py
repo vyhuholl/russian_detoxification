84ac1347226380b4a286cb666154ddb4c4dbc0d0
vyhuholl <olga-p-98@mail.ru>
2022-03-21 08:29:19 +0300

    wrote Detoxifier class

--- models/__init__.py
+++ models/__init__.py
@@ -15,6 +15,10 @@ from tqdm import tqdm
 from ufal.udpipe import Model, Pipeline
 from ..utils import show_progress
 
+if torch.cuda.is_available():
+    DEVICE = torch.device("cuda")
+else:
+    DEVICE = torch.device("cpu")
 
 fasttext_model_url = "http://vectors.nlpl.eu/repository/20/213.zip"
 fasttext_filename = "ru_fasttext/model.model"
@@ -33,3 +37,16 @@ udpipe_filename = udpipe_url.split("/")[-1]
 if not os.path.exists(udpipe_filename):
     print("UDPipe model not found. Downloading...", file=sys.stderr)
     urlretrieve(udpipe_url, udpipe_filename, show_progress)
+
+
+class Detoxifier:
+    def __init__(self, model_class: str, clf_path: str):
+        self.name = MODEL_CLASSES[model_class][2]
+        self.model = MODEL_CLASSES[model_class][0].from_pretrained(self.name)
+        self.model.to(DEVICE)
+        self.tokenizer = MODEL_CLASSES[model_class][1].from_pretrained(
+            self.name
+        )
+
+        with open(clf_path, "rb") as clf_file:
+            self.clf = pickle.load(clf_file)
